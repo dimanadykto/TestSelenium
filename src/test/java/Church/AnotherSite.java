f0ca086b8d7fda457611abb7bd07feb0e40d980c
dimanadykto <nadyktoo2@gmail.com>
2024-08-15 13:09:54 -0400

    implicit commit

--- src/test/java/Church/AnotherSite.java
+++ src/test/java/Church/AnotherSite.java
@@ -5,15 +5,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class AnotherSite {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.w3schools.com/");
-        Thread.sleep(4000);
+        Thread.sleep(2000);
         WebElement button =  driver.findElement(By.xpath("//a[text()='SQL Reference']"));
         button.click();
-        driver.quit();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.close();
     }
 }
--- src/test/java/Church/Locators.java
+++ src/test/java/Church/Locators.java
@@ -8,8 +8,6 @@ public class Locators {
         WebDriver driver = new ChromeDriver();
         driver.get("https://homeofgospel.org/");
         driver.manage().window().maximize();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,1000);");
         WebElement button = driver.findElement(By.xpath("//a[text()='Submit']"));
         button.click();
     }
