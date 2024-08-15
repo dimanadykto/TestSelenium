a7112d3bdc7ad0dc61370a1e1645990ecb621492
dimanadykto <nadyktoo2@gmail.com>
2024-08-15 13:24:13 -0400

    commmit 4

--- src/test/java/Church/RadioButtons.java
+++ src/test/java/Church/RadioButtons.java
@@ -1,4 +1,9 @@
 package Church;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RadioButtons {
+    WebDriver driver = new ChromeDriver();
+    
 }
